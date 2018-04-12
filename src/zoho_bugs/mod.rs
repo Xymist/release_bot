1603d8e490c876c6d73a7344feae3c62bdc9b5a5
Xymist <james@jamieduerden.me>
2018-04-12 21:46:37 +0100

    Fmt

--- src/zoho_bugs/mod.rs
+++ src/zoho_bugs/mod.rs
@@ -7,7 +7,7 @@ use errors::*;
 use std::{collections::HashMap, rc::Rc};
 use zohohorrorshow::client::ZohoClient;
 
-type ClassActions = HashMap<String, Vec<String>>;
+type ClassifiedActions = HashMap<String, Vec<String>>;
 
 pub fn zh_client(project_id: i64, config: &Config) -> Result<Rc<ZohoClient>> {
     let mut client = ZohoClient::new(
@@ -22,7 +22,7 @@ pub fn zh_client(project_id: i64, config: &Config) -> Result<Rc<ZohoClient>> {
     Ok(client)
 }
 
-pub fn classify_bugs(issues: IssueList) -> ClassActions {
+pub fn classify_bugs(issues: IssueList) -> ClassifiedActions {
     let bugs = issues.bugs;
     let buglist: Vec<Issue> = bugs.into_iter().filter(|bug| bug.is_closed()).collect();
     let mut client_list: HashMap<String, Vec<String>> = HashMap::new();
@@ -50,7 +50,7 @@ pub fn classify_bugs(issues: IssueList) -> ClassActions {
     client_list
 }
 
-pub fn classify_tasks(task_list: TaskList) -> ClassActions {
+pub fn classify_tasks(task_list: TaskList) -> ClassifiedActions {
     let tasks = task_list.tasks;
     let tasklist: Vec<Task> = tasks.into_iter().filter(|task| task.is_closed()).collect();
     let mut client_list: HashMap<String, Vec<String>> = HashMap::new();
@@ -77,7 +77,10 @@ pub fn classify_tasks(task_list: TaskList) -> ClassActions {
     client_list
 }
 
-pub fn merge_actions(issue_list: ClassActions, mut task_list: ClassActions) -> ClassActions {
+pub fn merge_actions(
+    issue_list: ClassifiedActions,
+    mut task_list: ClassifiedActions,
+) -> ClassifiedActions {
     for (client, mut bugs) in issue_list {
         let client_tasks = task_list.entry(client).or_insert(Vec::new());
         client_tasks.append(&mut bugs)
@@ -85,7 +88,7 @@ pub fn merge_actions(issue_list: ClassActions, mut task_list: ClassActions) -> C
     task_list
 }
 
-pub fn print_actions(mut client_list: ClassActions) -> Result<()> {
+pub fn print_actions(mut client_list: ClassifiedActions) -> Result<()> {
     let features = match client_list.remove("New Features") {
         Some(fs) => fs,
         None => Vec::new(),
